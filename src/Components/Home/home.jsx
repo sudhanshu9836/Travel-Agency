f6598d0aa2a6cd3e8638c4887e2bd5e56ac4206d
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-04-27 00:31:32 +0530

    Completed header and segment1 of home

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import "./home.css"
+import Card from './card'
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <div className="segment1">
+      <h3>------ Popular destinations</h3>
+      <div id='segment1-heading'>TOP NOTCH DESTINATIONS</div>
+        <div className="cards">
+          <Card
+          city = {"Udaipur"}
+          name= {"Lake Pichola"}
+          rating= {"★★★★☆"}
+          image={"https://i.pinimg.com/564x/6e/e1/da/6ee1da1b9d59ac20d01d66d2637346f9.jpg"}
+          />
+          <Card
+          city={"Varanasi"}
+          name={"Manikarnika Ghat"}
+          rating={"★★★★★"}
+          image={"https://i.pinimg.com/564x/07/b0/fa/07b0faa9315c357a09c6f9fde9745c23.jpg"}
+          />
+          <Card
+          city={"Goa"}
+          name={"Palolem Beach"}
+          rating={"★★★☆☆"}
+          image={"https://i.pinimg.com/564x/a6/74/f4/a674f407d8af1d8987e6599c0e51d5bc.jpg"}
+          />
+        </div>
+      </div>
     </div>
   )
 }
